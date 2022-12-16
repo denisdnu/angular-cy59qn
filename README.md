b174c94f67757d2e2577fdfec2ac5818da5c4176
denisdnu <denisdnu@gmail.com>
2022-12-16 22:51:39 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-cy59qn
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-cy59qn)
---
+++ src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { Product } from '../products';
+x
+@Component({
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css']
+})
+export class ProductAlertsComponent {
+
+  @Input() product!: Product;
+
+}
